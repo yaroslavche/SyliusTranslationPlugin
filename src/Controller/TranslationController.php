6def23004c5d7448e1e07c61c4d4d2af147b1b02
yaroslavche <yaroslav429@gmail.com>
2018-05-13 03:42:05 +0300

    templates, routes

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -11,6 +11,18 @@ use Symfony\Component\HttpFoundation\Response;
 final class TranslationController extends Controller
 {
     /**
+     * @return Response
+     */
+    public function dashboardAction(): Response
+    {
+        $translationPlugin = $this->get('translation_plugin_service');
+        $translationPlugin->setLocale();
+        return $this->render('@AcmeSyliusTranslationPlugin/dashboard.html.twig', [
+            'plugin' => $translationPlugin
+        ]);
+    }
+
+    /**
      * @param string|null $localeCode
      * @return Response
      */
@@ -18,8 +30,6 @@ final class TranslationController extends Controller
     {
         $translationPlugin = $this->get('translation_plugin_service');
         $translationPlugin->setLocaleByCode($localeCode);
-        // $translationPlugin->checkTranslations();
-
         return $this->render('@AcmeSyliusTranslationPlugin/translation.html.twig', [
             'plugin' => $translationPlugin
         ]);
