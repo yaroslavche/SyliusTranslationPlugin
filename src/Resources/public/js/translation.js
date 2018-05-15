a841b8a273016f0cb4dd87b1544735dcfd975a5c
yaroslavche <yaroslav429@gmail.com>
2018-05-15 14:22:11 +0300

    0.1.0

--- src/Resources/public/js/translation.js
+++ src/Resources/public/js/translation.js
@@ -1,25 +1,50 @@
-jQuery(document).ready(function() {
-    jQuery('.progress').progress();
-});
+var SyliusTranslationPlugin = {
+    editTranslation: (dataContainer) => {
+        let
+            localeCode = dataContainer.data('locale-code'),
+            domain = dataContainer.data('domain'),
+            messageDomain = dataContainer.data('message-domain'),
+            translation = dataContainer.data('translation'),
+            newTranslation = dataContainer.find('.translation_input').val();
 
-function stpEditMessage(localeCode, domain, messageDomain, translation)
-{
-    if(newTranslation = prompt(messageDomain + '\n(' + localeCode + ')', translation))
-    {
-        jQuery.post(
-            '/admin/translation/set',
-            {
-                'localeCode': localeCode,
-                'domain': domain,
-                'messageDomain': messageDomain,
-                'translation': newTranslation
-            }, function(response) {
-                if(response.status == 'success')
-                {
-                    alert('ok');
-                    // change data
-                }
-                else alert('something wrong');
-            }, 'json');
+        if (translation == newTranslation) return;
+        dataContainer.find('.edit.link.icon').addClass('loading');
+        SyliusTranslationPlugin.setMessage(localeCode, domain, messageDomain, newTranslation, (result) => {
+            dataContainer.find('.edit.link.icon').removeClass('loading');
+            if (result) {
+                if (translation == '')
+                    dataContainer.find('.icon.circle.outline.red').removeClass('red').addClass('check green');
+                if (dataContainer.find('.icon.cog.teal').length == 0)
+                    dataContainer.find('.icon.circle.check.green').after('<i class="icon cog teal"></i>');
+            } else {
+                alert('something wrong');
+            }
+        });
+    },
+    setMessage: (localeCode, domain, messageDomain, translation, callback) => {
+        let result = false;
+        jQuery.ajax({
+                method: 'POST',
+                url: '/admin/translation/set',
+                data: {
+                    'localeCode': localeCode,
+                    'domain': domain,
+                    'messageDomain': messageDomain,
+                    'translation': translation
+                },
+                dataType: 'json'
+            })
+            .done(function(response) {
+                callback(response.status == 'success');
+            });
     }
-}
+};
+
+jQuery(document).ready(function() {
+    if (jQuery('.progress').length > 0) jQuery('.progress').progress();
+    jQuery('.translation_input').keypress(function(e) {
+        if (e.which == 13) {
+            SyliusTranslationPlugin.editTranslation(jQuery(this).parent().parent().parent());
+        }
+    });
+});
